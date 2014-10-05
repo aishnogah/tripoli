d0fa3c1bda938e545feebc269e353b26c931faf7
Benjamin Snyder <bsnyder@Benjamins-MacBook-Pro.local>
2014-10-05 13:49:26 -0500

    reorganized

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  triploli
+//
+//  Created by Benjamin Snyder on 10/5/14.
+//  Copyright (c) 2014 Snyder. All rights reserved.
+//
+
+#include "tripoli.h"
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
